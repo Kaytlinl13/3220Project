ddb953291426ca9d6a36f562605b1c0545bcfa88
JLSmith16 <42877342+JLSmith16@users.noreply.github.com>
2018-12-02 14:31:30 -0600

    Update ChooseRace.vue

--- frontend/src/pages/chooseRace/ChooseRace.vue
+++ frontend/src/pages/chooseRace/ChooseRace.vue
@@ -15,29 +15,36 @@
 								<v-btn v-else style="color:white;" block color="#5e3fb4" v-on:click="whichRace=0">Dragonborn</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Dwarf</v-btn>
+								<v-btn v-if="whichRace == 1" style="color:white;" block color="#9a35b7">Dwarf</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=1">Dwarf</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Elf</v-btn>
+								<v-btn v-if="whichRace == 2" style="color:white;" block color="#9a35b7">Elf</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=2">Elf</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Gnome</v-btn>
+								<v-btn v-if="whichRace == 3" style="color:white;" block color="#9a35b7">Gnome</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=3">Gnome</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn v-if="whichRace == 5" style="color:white;" block color="#9a35b7">Half-Elf</v-btn>
-								<v-btn v-else style="color:white;" block color="#5e3fb4" v-on:click="whichRace=5">Half-Elf</v-btn>
+								<v-btn v-if="whichRace == 4" style="color:white;" block color="#9a35b7">Half-Elf</v-btn>
+								<v-btn v-else style="color:white;" block color="#5e3fb4" v-on:click="whichRace=4">Half-Elf</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Halfling</v-btn>
+								<v-btn v-if="whichRace == 5" style="color:white;" block color="#9a35b7">Halfling</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=5">Halfling</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Half-Orc</v-btn>
+								<v-btn v-if="whichRace == 6" style="color:white;" block color="#9a35b7">Half-Orc</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=6">Half-Orc</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Human</v-btn>
+								<v-btn v-if="whichRace == 7" style="color:white;" block color="#9a35b7">Human</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=7">Human</v-btn>
 							</v-flex>
 							<v-flex ma-2>
-								<v-btn style="color:white;" block color="#5e3fb4">Tiefling</v-btn>
+								<v-btn v-if="whichRace == 8" style="color:white;" block color="#9a35b7">Tiefling</v-btn>
+								<v-btn v-else style="color:white;" block color="5e3fb4" v-on:click="whichRace=8">Tiefling</v-btn>
 							</v-flex>
 						</v-card>
 						<v-flex>
@@ -57,7 +64,31 @@
 								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
 							</v-flex>
 						</v-card>
-						<v-card v-if="whichRace == 5">
+						<v-card v-if="whichRace == 1">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Dwarf</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/dwarf.jpg')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutDwarf}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 2">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Elf</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/elf.png')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutElf}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 3">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Gnome</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/gnome.png')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutGnome}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 4">
 							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Half-Elf</h1>
 							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/halfelf.png')"></v-img>
 							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutHalfElf}}</p>
@@ -65,13 +96,45 @@
 								<v-btn block color="#fece56" to='NameCharacter'>Next: Name, Level, and Alignment</v-btn>
 							</v-flex>
 						</v-card>
+						<v-card v-if="whichRace == 5">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Halfling</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/halfling.jpg')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutHalfling}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 6">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Half-Orc</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/halforc.png')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutHalfOrc}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 7">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Human</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/human.png')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutHuman}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
+						<v-card v-if="whichRace == 8">
+							<h1 style="font-family:milonga; font-size:300%; text-align: center;">Tiefling</h1>
+							<v-img contain aspect-ratio="2" v-bind:src="require('../../assets/images/tiefling.jpg')"></v-img>
+							<p style="font-size:200%; text-align:left; padding: 40px; text-indent: 25px;">{{aboutTiefling}}</p>
+							<v-flex>
+								<v-btn block color="#fece56" to='/NameCharacter'>Next: Name, Level, and Alignment</v-btn>
+							</v-flex>
+						</v-card>
 					</v-flex>
 				</v-card>
 			</v-flex>
 		</v-layout>
-	
 
-	
+
+
 	</v-container>
 </template>
 
@@ -85,7 +148,14 @@ import Navigation from "../universalComponents/navigation.vue"
 		data:() => ({
 			whichRace: 0,
 			aboutDragonborn: "Dragonborn look very much like dragons standing tall in humanoid form, though they lack wings or tails. A strong and charismatic race, they do tend to face discrimination by the more common races.",
-			aboutHalfElf: "Half-Elves combine what some say are the best qualities of their elf and human parents. Half-elves live longer than their human counterparts, but have the natural charisma of humans."
+			aboutDwarf: "Dwarves are solid and enduring like the mountains they love, weathering the passage of centuries with stoic endurance and little change.",
+			aboutElf: "Elves are a magical people of otherworldly grace, living in the world but not entirely part of it.",
+			aboutGnome: "Gnomes take delight in life, enjoying every moment of invention, exploration, investigation, creation, and play.",
+			aboutHalfElf: "Half-Elves combine what some say are the best qualities of their elf and human parents. Half-elves live longer than their human counterparts, but have the natural charisma of humans.",
+			aboutHalfling: "The comforts of home are the goals of most halflings’ lives: a place to settle in peace and quiet, far from marauding monsters and clashing armies.",
+			aboutHalfOrc: "Half-orcs are not evil by nature, but evil does lurk within them, whether they embrace it or rebel against it.",
+			aboutHuman: "Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.",
+			aboutTiefling: "Their appearance and their nature are not their fault but the result of an ancient sin, for which they and their children and their children’s children will always be held accountable."
 		})
 	}
 </script>
